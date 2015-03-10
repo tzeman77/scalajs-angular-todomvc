a94ad90d2f93ef57b0c0416be9a94c9628d3b4ec
Tomas Zeman <tzeman@volny.cz>
2015-03-10 12:23:35 +0100

    BulletCtrl: moved Data -> controller

--- scalajs/src/main/scala/todomvc/example/BulletCtrl.scala
+++ scalajs/src/main/scala/todomvc/example/BulletCtrl.scala
@@ -22,17 +22,18 @@ case class Chart(var `type`: String, var transitionDuration: Int = 500)
 @JSExportAll
 case class Options(var chart: Chart)
 
-@JSExportAll
-case class Data(var title: String, var subtitle: String, var ranges: js.Array[Int],
-  var measures: js.Array[Int], var markers: js.Array[Int])
-
 @JSExport
 object BulletCtrl extends Controller {
+  @JSExportAll
+  case class Data(var title: String, var subtitle: String, var ranges: js.Array[Int],
+    var measures: js.Array[Int], var markers: js.Array[Int])
+
   override def initialize(scope: ScopeType) {
     scope.options = Options(Chart("bulletChart"))
     scope.data = Data("Temperature", "deg. C", js.Array(30, 50, 120), js.Array(37),
       js.Array(100))
   }
+
 
   trait ScopeType extends Scope {
 
