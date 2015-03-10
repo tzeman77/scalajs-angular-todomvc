e89d093469dee315bfc5d375f3c1f733f9890d52
Tomas Zeman <tzeman@volny.cz>
2015-03-10 13:34:22 +0100

    Charts DSL

--- scalajs/src/main/scala/todomvc/example/BulletCtrl.scala
+++ scalajs/src/main/scala/todomvc/example/BulletCtrl.scala
@@ -16,31 +16,53 @@ import com.greencatsoft.angularjs.inject
 import com.greencatsoft.angularjs.Controller
 import com.greencatsoft.angularjs.core.{ Location, Scope }
 
-@JSExportAll
-case class Chart(var `type`: String, var transitionDuration: Int = 500)
+class Chart(body: Map[String, Any]) {
+  def transitionDuration(ms: Int) = new Chart(body + ("transitionDuration" -> ms))
+  def height(h: Int) = new Chart(body + ("height" -> h))
+  import js.JSConverters._
+  def toJs = body.toJSDictionary
+}
+
+object Chart {
+  def bulletChart = new Chart(Map("type" -> "bulletChart"))
+}
 
 @JSExportAll
-case class Options(var chart: Chart)
+case class Options(var chart: js.Dictionary[Any])
+
+class ChartData(body: Map[String, Any] = Map()) {
+  def title(t: String) = new ChartData(body + ("title" -> t))
+  def subtitle(s: String) = new ChartData(body + ("subtitle" -> s))
+
+  /* bullet chart */
+  import js.JSConverters._
+  def ranges(is: Int*) = new ChartData(body + ("ranges" -> is.toJSArray))
+  def measures(is: Int*) = new ChartData(body + ("measures" -> is.toJSArray))
+  def markers(is: Int*) = new ChartData(body + ("markers" -> is.toJSArray))
+
+  def toJs = body.toJSDictionary
+}
+
+object ChartData {
+  def apply() = new ChartData
+}
+
+trait ChartScope extends Scope {
+  var options: Options = js.native
+  var data: js.Dictionary[Any] = js.native
+}
 
 @JSExport
 object BulletCtrl extends Controller {
-  @JSExportAll
-  case class Data(var title: String, var subtitle: String, var ranges: js.Array[Int],
-    var measures: js.Array[Int], var markers: js.Array[Int])
 
   override def initialize(scope: ScopeType) {
-    scope.options = Options(Chart("bulletChart"))
-    scope.data = Data("Temperature", "deg. C", js.Array(30, 50, 120), js.Array(37),
-      js.Array(100))
+    scope.options = Options(Chart.bulletChart transitionDuration(500) toJs)
+    scope.data = (ChartData() title("Temperature") subtitle("deg. C")
+      ranges(30, 60, 120) measures(37) markers(100) toJs)
   }
 
+  trait ScopeType extends ChartScope
 
-  trait ScopeType extends Scope {
-
-    var options: Options = js.native
-
-    var data: Data = js.native
-  }
 }
 
 
